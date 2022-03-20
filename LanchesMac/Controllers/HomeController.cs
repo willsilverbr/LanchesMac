d69bd14763bc58a31bb0764fbccea05a4e0779b0
William Barbosa <will.silver@hotmail.com>
2022-03-20 00:49:32 -0300

    Add project files.

---
+++ LanchesMac/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using LanchesMac.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace LanchesMac.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+       
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
